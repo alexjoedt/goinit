1bc3032233f6cd779cd36d560cf2d5575cb331df
Alex Joedt <84435160+alexjoedt@users.noreply.github.com>
2026-03-12 06:44:38 +0100

    refactor(logger.go): update function signatures to use 'any' type

--- logger.go
+++ logger.go
@@ -6,19 +6,19 @@ import (
 )
 
 // info prints informational messages only when verbose mode is enabled
-func info(config *Config, format string, args ...interface{}) {
+func info(config *Config, format string, args ...any) {
 	if config != nil && config.Verbose {
 		fmt.Fprintf(os.Stderr, "• "+format+"\n", args...)
 	}
 }
 
 // warn prints warning messages to stderr - always shown
-func warn(format string, args ...interface{}) {
+func warn(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, "Warning: "+format+"\n", args...)
 }
 
 // fatal prints error message to stderr and exits with code 1
-func fatal(format string, args ...interface{}) {
+func fatal(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, "Error: "+format+"\n", args...)
 	os.Exit(1)
 }
